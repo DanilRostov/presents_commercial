a24e4977a959f12cbbc53cdb85d5e63cc75189b2
Danil <daniil.rossy@gmail.com>
2018-09-09 18:18:56 +0300

    added run script

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 "use strict"
-const express = require('express');
-const mongoose = require('mongoose');
-const port = process.env.PORT || 5000;
-const bodyParser = require('body-parser');
-const db = require('./config/keys').mongoURI;
-const app = express();
 const path = require('path');
 const fs = require('fs');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
 const cors = require('cors');
 
+const db = require('./config/keys').mongoURI;
+const port = process.env.PORT || 5000;
 const toys = require('./routes/toys');
 const bags = require('./routes/bags');
 
@@ -44,5 +45,15 @@ app.get('/images/:page/:id', function (req, res) {
     }
   });
 });
+
+// Serve static asserts if in production
+if(precess.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
