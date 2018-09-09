86b748fc1b9f6fef3778f497308221c1cf66bae1
Danil <daniil.rossy@gmail.com>
2018-09-09 18:45:22 +0300

    version 0.0.1

--- server.js
+++ server.js
@@ -47,12 +47,12 @@ app.get('/images/:page/:id', function (req, res) {
 });
 
 // Serve static asserts if in production
-if(precess.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
