40737cacbaeda36f2c1a9262e6778da0e3ed4d64
Danil <daniil.rossy@gmail.com>
2018-09-18 00:02:20 +0300

    added env

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,17 @@
+// module.exports = {
+//   mongoURI: 'mongodb+srv://mainUser:v0Tw704e0VhJ7e6O@gifts-cluster-unds5.mongodb.net/myDatabase'
+// }
+
+"use strict"
+const {
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
+  DB_ID,
+  TOKEN_SECRET
+} = process.env;
+
 module.exports = {
-  mongoURI: 'mongodb+srv://mainUser:v0Tw704e0VhJ7e6O@gifts-cluster-unds5.mongodb.net/myDatabase'
+  mongoURI: `${DB_NAME}://${DB_USER}:${DB_PASS}@${DB_ID}`
+  // secret: TOKEN_SECRET
 }
