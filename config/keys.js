0119132db38a8277236008125fc269466a9ea789
Danil <daniil.rossy@gmail.com>
2018-09-19 10:28:09 +0300

    removed unused code

--- config/keys.js
+++ config/keys.js
@@ -1,17 +1,11 @@
-// module.exports = {
-//   mongoURI: 'mongodb+srv://mainUser:v0Tw704e0VhJ7e6O@gifts-cluster-unds5.mongodb.net/myDatabase'
-// }
-
 "use strict"
 const {
   DB_NAME,
   DB_USER,
   DB_PASS,
-  DB_ID,
-  TOKEN_SECRET
+  DB_ID
 } = process.env;
 
 module.exports = {
   mongoURI: `${DB_NAME}://${DB_USER}:${DB_PASS}@${DB_ID}`
-  // secret: TOKEN_SECRET
 }
